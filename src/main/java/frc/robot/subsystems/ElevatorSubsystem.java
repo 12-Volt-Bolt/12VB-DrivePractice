f0404eeb925c6d11b5e9c728d7b9c3569029520d
11VZ <carrytacker@gmail.com>
2025-03-16 18:23:30 -0400

    Add files via upload

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private final SparkMax motor = new SparkMax(Constants.ELEVATOR_MOTOR_ID, MotorType.kBrushless);
+    private final XboxController controller = new XboxController(0);
+    
+    private static final double GEAR_RATIO = Constants.ELEVATOR_GEAR_RATIO;
+    private static final double SPOOL_DIAMETER_INCHES = Constants.ELEVATOR_SPOOL_DIAMETER;
+    private static final double MAX_HEIGHT_INCHES = Constants.ELEVATOR_MAX_HEIGHT;
+    private static final double MIN_HEIGHT_INCHES = Constants.ELEVATOR_MIN_HEIGHT;
+    
+    private double speed = 0.0;
+
+    public double getMotorRotations() {
+        return motor.getEncoder().getPosition();
+    }
+    
+    public double getMotorVelocity() {
+        return motor.getEncoder().getVelocity();
+    }
+    
+    public void setSpeedManual(double speed) {
+        motor.set(speed);
+    }
+
+    @Override
+    public void periodic() {
+        boolean leftBumper = controller.getRawButton(5);
+        boolean rightBumper = controller.getRawButton(6);
+        double currentPosition = (motor.getEncoder().getPosition() / GEAR_RATIO) * 
+                               (Math.PI * SPOOL_DIAMETER_INCHES);
+
+        SmartDashboard.putBoolean("Controller: Left Bumper", false);
+        SmartDashboard.putBoolean("Controller: Right Bumper", false);
+
+        if (leftBumper && rightBumper) {
+            speed = 0.0;
+            SmartDashboard.putBoolean("Controller: Right Bumper", false);
+            SmartDashboard.putBoolean("Controller: Left Bumper", false);
+        } else if (leftBumper) {
+            SmartDashboard.putBoolean("Controller: Left Bumper", true);
+            speed = Constants.ELEVATOR_SPEED;
+        } else if (rightBumper) {
+            SmartDashboard.putBoolean("Controller: Right Bumper", true);
+            speed = -Constants.ELEVATOR_SPEED;
+        } else {
+            speed = 0.0;
+        }
+
+        SmartDashboard.putNumber("Elevator Position (inches)", currentPosition);
+        SmartDashboard.putBoolean("Elevator At Max Height", currentPosition >= MAX_HEIGHT_INCHES);
+        SmartDashboard.putBoolean("Elevator At Min Height", currentPosition <= MIN_HEIGHT_INCHES);
+        
+        motor.set(speed);
+    }
+}
